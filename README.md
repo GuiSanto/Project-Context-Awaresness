61bbfb540c45821f25893bfb0052903f61b52a72
GuiSanto <guiontheworks@gmail.com>
2022-05-20 11:19:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Project-Context-Awaresness"  
---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,45 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+
+        //The complete onClickListener with Navigation
+        binding.playButton.setOnClickListener { view : View ->
+            /*
+            FAZER BOTÃO
+             */
+        }
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    // navigate to the Fragment that has the same id as the selected menu item
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+
+}
